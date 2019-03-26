ebb2546f135977971ddf9bc9cd7b641310e59121
Farshid Felfelani <felfelan@egr.msu.edu>
2019-03-26 09:50:29 -0600

    Nothing Important! Just a change in a comment!

--- src/main/decompMod.F90
+++ src/main/decompMod.F90
@@ -114,7 +114,7 @@ module decompMod
   !--- gdc = 1d global dc ordered compressed
   type decomp_type
      integer,pointer :: gdc2glo(:)    ! 1d gdc to 1d glo
-     integer,pointer :: ixy(:)        ! i and j indices
+     integer,pointer :: ixy(:)        ! FFelfelani Comment: i and j indices
      integer,pointer :: jxy(:)        ! relative to the grid cell vector
   end type decomp_type
   public decomp_type
