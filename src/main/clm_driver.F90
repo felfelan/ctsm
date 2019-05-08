c40300cd4cff2348b8bd83a40e0d3e6a4fb28614
Farshid Felfelani <felfelan@egr.msu.edu>
2019-05-08 12:07:19 -0600

    Pass over the irrigation_inst in the HydrologyNoDrainage subroutine
    To use the qflx_irrig_col from the irrigation_inst class in
    biogeophys/GroundwaterMod.F90, I need to pass it over from clm_driver.F90
    to HydrologyNoDrainageMod.F90 to WaterTable subroutine of SoilHydrologyMod.F90

--- src/main/clm_driver.F90
+++ src/main/clm_driver.F90
@@ -701,7 +701,7 @@ contains
             clm_fates,                                                         &
             atm2lnd_inst, soilstate_inst, energyflux_inst, temperature_inst,   &
             waterflux_inst, waterstate_inst, soilhydrology_inst, aerosol_inst, &
-            canopystate_inst, soil_water_retention_curve, topo_inst)
+            canopystate_inst, soil_water_retention_curve, topo_inst, irrigation_inst)
 
        ! The following needs to be done after HydrologyNoDrainage (because it needs
        ! waterflux_inst%qflx_snwcp_ice_col), but before HydrologyDrainage (because
