0a725bd89c48a32e41de8d3b1a71099e7a7e91f2
Farshid Felfelani <felfelan@egr.msu.edu>
2019-12-06 09:20:32 -0700

    Inserting a new GW module for CLM Default: No lateral flow + Pumping

--- src/biogeophys/GroundwaterMod.F90
+++ src/biogeophys/GroundwaterMod.F90
@@ -67,6 +67,7 @@ module GroundwaterMod
      ! Public routines
      procedure, public :: UpdateGWFanLatPump
      procedure, public :: UpdateGWFanLatTheimPump
+     procedure, public :: UpdateGWDefaultPump
      ! Private routines
      procedure, private :: TransmissivityFromFan
      procedure, private :: TheimLateral
@@ -1213,6 +1214,187 @@ contains
      end associate
   end subroutine UpdateGWFanLatTheimPump
 
+  !-----------------------------------------------------------------------
+  !------------------------------------------------------------------------
+  subroutine UpdateGWDefaultPump(this, bounds, num_hydrologyc, filter_hydrologyc, &
+        soilhydrology_inst, soilstate_inst,waterstate_inst, irrigation_inst)
+
+    ! !DESCRIPTION:
+    !   In principle, Theim equation is applied between 
+    !   r = r_e and r = dx (center to center of four
+    !   surrounding cells) to take into account the GW pumping.
+    !
+    !   Assumptions: The head across a given radius is constant 
+    !  (i.e., cone of depression is circular) 
+
+    ! !USES:
+    use spmdMod         , only : MPI_REAL8, MPI_SUM, mpicom, MPI_INTEGER
+    use decompMod       , only : ldecomp, get_proc_global
+    use shr_const_mod   , only : SHR_CONST_PI
+    use GridcellType    , only : grc
+    use clm_time_manager, only : get_step_size, get_curr_date, get_nstep
+    use landunit_varcon , only : istwet, istsoil, istice_mec, istcrop
+
+    ! !ARGUMENTS:
+    class(groundwater_type)  , intent(inout) :: this
+    type(bounds_type)        , intent(in)    :: bounds  
+    integer                  , intent(in)    :: num_hydrologyc       ! number of column soil points in column filter
+    integer                  , intent(in)    :: filter_hydrologyc(:) ! column filter for soil points
+    type(soilhydrology_type) , intent(inout) :: soilhydrology_inst
+    type(soilstate_type)     , intent(in)    :: soilstate_inst
+    type(waterstate_type)    , intent(inout) :: waterstate_inst
+    type(irrigation_type)    , intent(in)    :: irrigation_inst	
+
+    ! !LOCAL VARIABLES:	
+ 
+    character(len=32) :: subname = 'GroundwaterMod' ! subroutine name 
+
+    real(r8) :: rous, aRatio, colArea              ! aquifer yield (-); area ratio of center/neighbor
+    real(r8) :: s_y, dummysum, dummysum2
+    real(r8) :: pump_tot, pump_layer
+    integer  :: jwt(bounds%begc:bounds%endc)       ! index of the soil layer right above the water table (-)
+    real(r8) :: dtime                              ! land model time step (sec)
+
+	
+    integer :: ng, nl, nc, np, nCohorts            ! total number of grid cells,landunits,columns,patches
+    integer :: g, c                                ! patch, gridcell, column indices
+    integer :: ier                                 ! error code
+    integer :: j,fc,i
+
+    integer :: begg, endg       ! beginning and ending gridcell index of current proc
+    integer :: begl, endl       ! beginning and ending landunit index of current proc
+    integer :: begc, endc       ! beginning and ending column index of current proc
+    integer :: begp, endp       ! beginning and ending pft index of current proc
+
+    integer :: year       ! year (0, ...) for nstep
+    integer :: month      ! month (1, ..., 12) for nstep
+    integer :: day        ! day of month (1, ..., 31) for nstep
+    integer :: secs       ! seconds into current date for nstep
+    integer :: nstep
+
+    ! Conversion factors
+
+    real(r8), parameter :: km_to_mm    = 1.e6_r8
+    real(r8), parameter :: km2_to_mm2  = 1.e12_r8
+    real(r8), parameter :: mm_to_m     = 1.e-3_r8
+    real(r8), parameter :: m_to_mm     = 1.e3_r8
+    real(r8), parameter :: HydroThresh = 0.1_r8
+    !-----------------------------------------------------------------------
+     associate(&                               
+          zi                 =>    col%zi                                , & ! Input:  [real(r8) (:,:) ]  interface level below a "z" level (m)           
+          GW_ratio           =>    col%GW_ratio                          , & ! Input:  [real(r8) (:)   ]  USGS GW ratio as irrigation source                                                
+                                              
+          qflx_irrig         =>    irrigation_inst%qflx_irrig_col        , & ! irrigation flux (mm H2O /s)
+
+          bsw                =>    soilstate_inst%bsw_col                , & ! Input:  [real(r8) (:,:) ]  Clapp and Hornberger "b"                        
+          hksat              =>    soilstate_inst%hksat_col              , & ! Input:  [real(r8) (:,:) ]  hydraulic conductivity at saturation (mm H2O /s)
+          sucsat             =>    soilstate_inst%sucsat_col             , & ! Input:  [real(r8) (:,:) ]  minimum soil suction (mm)                       
+          watsat             =>    soilstate_inst%watsat_col             , & ! Input:  [real(r8) (:,:) ]  volumetric soil water at saturation (porosity)
+
+          zwt                =>    soilhydrology_inst%zwt_col                , & ! Input and Output: [real(r8) (:)   ]  water table depth (m)
+          wa                 =>    soilhydrology_inst%wa_col                 , & ! Output: [real(r8) (:)   ]  water in the unconfined aquifer (mm)
+          qcharge            =>    soilhydrology_inst%qcharge_col            , & ! Input:  [real(r8) (:)   ]  aquifer recharge rate (mm/s)
+          Qgw_lateral        =>    soilhydrology_inst%Qgw_lateral_col        , & ! Output: [real(r8) (:)   ]  GW lateral flow (mm/s)
+          AqTransmiss        =>    soilhydrology_inst%AqTransmiss_col        , & ! Output: [real(r8) (:)   ]  Aquifer Transmissivity(mm2/s)
+          Pump_wa            =>    soilhydrology_inst%Pump_wa_col            , & ! Output: [real(r8) (:)   ]  Pumped Water from the aquifer(mm/s)
+          QlatField_north    =>    soilhydrology_inst%QlatField_northing_grc , & !  Output: [real(r8) (:)   ] Northward lateral GW flow
+          QlatField_east     =>    soilhydrology_inst%QlatField_easting_grc  , & !  Output: [real(r8) (:)   ] Eastward lateral GW flow
+
+          h2osoi_liq         =>    waterstate_inst%h2osoi_liq_col              & ! Output: [real(r8) (:,:) ] liquid water (kg/m2)
+          )
+       !-----------------------------------------------
+       dtime = get_step_size()
+       nstep = get_nstep()
+       call get_curr_date (year, month, day, secs)
+       call get_proc_global(ng=ng, nl=nl, nc=nc, np=np, nCohorts=nCohorts)
+
+
+       ! The layer index of the first unsaturated layer, i.e., the layer right above
+       ! the water table
+       do fc = 1, num_hydrologyc
+          c = filter_hydrologyc(fc) 
+          jwt(c) = nlevsoi
+          ! allow jwt to equal zero when zwt is in top layer  
+          do j = 1,nlevsoi
+             if(zwt(c) <= zi(c,j)) then
+                jwt(c) = j-1 
+                exit
+             end if
+          enddo
+       end do
+
+      ! Removing the pumped water from the soil column	   
+       if (use_pumping == .true.) then
+          do fc = 1, num_hydrologyc
+             c = filter_hydrologyc(fc)
+             g = col%gridcell(c)
+                 !!! use analytical expression for aquifer specific yield
+                 rous = watsat(c,nlevsoi) &
+                      * ( 1. - (1.+1.e3*zwt(c)/sucsat(c,nlevsoi))**(-1./bsw(c,nlevsoi)))
+                 rous=max(rous,0.02_r8)
+
+                 pump_tot = - GW_ratio(c) * qflx_irrig(c) * dtime
+                 Pump_wa(c) = GW_ratio(c) * qflx_irrig(c)
+                 ! if (nstep > 400 .and. c == 838456) write(*,*) 'c,', c, pump_tot, Pump_wa(c) 
+                 !!!--  water table is below the soil column  --------------------------------------
+                 if(jwt(c) == nlevsoi) then             
+                    wa(c)  = wa(c) + pump_tot
+                    zwt(c) = zwt(c) - pump_tot/1000._r8/rous
+ 
+                 else                                
+                    !!!-- water table within soil layers 1-9  --------------------------------------
+                    !!!============================== pump_tot ========================================= 
+                    !!!--  Now remove water via pump_tot
+
+                    !!!should never be positive... but include for completeness
+                    if(pump_tot > 0.) then !rising water table
+
+                       call endrun(msg="pump_tot IS POSITIVE in Groundwater!"//errmsg(sourcefile, __LINE__))
+
+                    else ! deepening water table
+                       do j = jwt(c)+1, nlevsoi
+                          !!! use analytical expression for specific yield
+                          s_y = watsat(c,j) &
+                               * ( 1. - (1.+1.e3*zwt(c)/sucsat(c,j))**(-1./bsw(c,j)))
+                          s_y=max(s_y,0.02_r8)
+
+                          pump_layer=max(pump_tot,-(s_y*(zi(c,j) - zwt(c))*1.e3))
+                          pump_layer=min(pump_layer,0._r8)
+                          h2osoi_liq(c,j) = h2osoi_liq(c,j) + pump_layer
+
+                          pump_tot = pump_tot - pump_layer
+
+                          if (pump_tot >= 0.) then 
+                             zwt(c) = zwt(c) - pump_layer/s_y/1000._r8
+                             exit
+                          else
+                             zwt(c) = zi(c,j)
+                          endif
+                       enddo
+
+                       !!!--  remove residual pump_tot  ---------------------------------------------
+                       zwt(c) = zwt(c) - pump_tot/1000._r8/rous
+                       wa(c)  = wa(c) + pump_tot
+                    endif
+
+                    !!!-- recompute jwt  ---------------------------------------------------------
+                    !!! allow jwt to equal zero when zwt is in top layer
+                    jwt(c) = nlevsoi
+                    do j = 1,nlevsoi
+                       if(zwt(c) <= zi(c,j)) then
+                          jwt(c) = j-1
+                          exit
+                       end if
+                    enddo
+                 end if! end of jwt if construct
+
+                 zwt(c) = max(0.0_r8,zwt(c))
+                 zwt(c) = min(80._r8,zwt(c))    
+          end do
+       end if
+
+     end associate
+  end subroutine UpdateGWDefaultPump
   !-----------------------------------------------------------------------
 
   function TransmissivityFromFan(this, bounds, num_hydrologyc, filter_hydrologyc, &
