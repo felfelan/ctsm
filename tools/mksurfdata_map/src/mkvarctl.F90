a38527be874c6be7e631359ec7acb431b69dcd34
Farshid Felfelani <felfelan@egr.msu.edu>
2020-10-06 11:50:44 -0600

    Adding control variables for USGS GW-SW and Fan WTD field!
    Two new fields are added to the surface data file, we need to
    add the control variables for them.

--- tools/mksurfdata_map/src/mkvarctl.F90
+++ tools/mksurfdata_map/src/mkvarctl.F90
@@ -50,6 +50,8 @@ module mkvarctl
   character(len=256), public :: mksrf_ftopostats = ' '  ! topography statistics data file name
   character(len=256), public :: mksrf_fvic       = ' '  ! VIC parameters data file name
   character(len=256), public :: mksrf_fch4       = ' '  ! inversion-derived CH4 parameters data file name
+  character(len=256), public :: mksrf_fWTD       = ' '  ! WTD file name
+  character(len=256), public :: mksrf_fUSGSGW    = ' '  ! USGS GW file name
 
   integer           , public :: numpft         = 16   ! number of plant types
 
@@ -74,6 +76,8 @@ module mkvarctl
   character(len=256), public :: map_ftopostats  = ' ' ! Mapping file for topography statistics
   character(len=256), public :: map_fvic        = ' ' ! Mapping file for VIC parameters
   character(len=256), public :: map_fch4        = ' ' ! Mapping file for inversion-derived CH4 parameters
+  character(len=256), public :: map_fWTD        = ' ' ! Mapping file for WTD
+  character(len=256), public :: map_fUSGSGW     = ' ' ! Mapping file for USGS GW
   character(len=80) , public :: gitdescribe     = ' ' ! Description of model version from git
 !
 ! Variables to override data read in with
