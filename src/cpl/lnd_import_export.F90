f4dad87447bb6eefbdd6c3fe6b86a5da8a8ba4cc
Farshid Felfelani <felfelan@egr.msu.edu>
2020-10-16 19:07:52 -0600

    Converting the data to be sent from the clm model to the coupler!
    NO3 flux sent individually to coupler
    l2x(index_l2x_Flrl_no3_runoff,i)  = lnd2atm_inst%smin_no3_runoff_grc(g)
    l2x(index_l2x_Flrl_no3_leached,i) = lnd2atm_inst%smin_no3_leached_grc(g)

--- src/cpl/lnd_import_export.F90
+++ src/cpl/lnd_import_export.F90
@@ -398,6 +398,10 @@ contains
        ! irrigation flux to be removed from main channel storage (negative)
        l2x(index_l2x_Flrl_irrig,i) = - lnd2atm_inst%qirrig_grc(g)
 
+       ! NO3 flux sent individually to coupler
+       l2x(index_l2x_Flrl_no3_runoff,i)  = lnd2atm_inst%smin_no3_runoff_grc(g)
+       l2x(index_l2x_Flrl_no3_leached,i) = lnd2atm_inst%smin_no3_leached_grc(g)
+
        ! glc coupling
        ! We could avoid setting these fields if glc_present is .false., if that would
        ! help with performance. (The downside would be that we wouldn't have these fields
